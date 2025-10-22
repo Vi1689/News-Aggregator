6f32167da53fdf42e47de68cbb966f3ebba8a9ab
WolfishTone <igorion98@gmail.com>
2025-10-22 20:50:04 +0700

    реализовано заполнение групп, постов, комментариев и медиа

--- researchers/vk/main.go
+++ researchers/vk/main.go
@@ -7,7 +7,6 @@ import (
 
 	"researcher-vk/internal/config"
 	"researcher-vk/internal/sendRequests"
-	_ "researcher-vk/internal/sendRequests"
 	"researcher-vk/internal/vk"
 	_ "researcher-vk/internal/vkToken"
 )
@@ -20,8 +19,8 @@ func MakeGroupsSlice(accessToken string, conf config.ResearcherConfig) ([]vk.VKG
 }
 
 func main() {
-	//timer1 := time.NewTimer(time.Duration(10) * time.Second)
-	//<-timer1.C
+	timer1 := time.NewTimer(time.Duration(10) * time.Second)
+	<-timer1.C
 
 	// автоматизация получения токена vk
 	/*go func() {
@@ -29,8 +28,8 @@ func main() {
 	}()*/
 	// вк = говно
 
-	//accessTokenFileName := "/usr/local/etc/vk-researcher/access_token"
-	accessTokenFileName := "access_token"
+	accessTokenFileName := "/usr/local/etc/vk-researcher/access_token"
+	//accessTokenFileName := "access_token"
 	accessToken, err := os.ReadFile(accessTokenFileName)
 	if err != nil {
 		fmt.Println(err)
@@ -38,8 +37,7 @@ func main() {
 	}
 
 	// Добавить источник VK в начале (один раз)
-	_, err = sendRequests.AddVKSource()
-	sourceID, err := sendRequests.GetSourceID()
+	sourceID, err := sendRequests.AddVKSource()
 	if err != nil {
 		fmt.Printf("Failed to add VK source: %v\n", err)
 		os.Exit(1)
@@ -57,8 +55,9 @@ func main() {
 			<-timer1.C
 			continue
 		}*/
-		//ConfigFileName := "/usr/local/etc/vk-researcher/test_conf.xml"
-		ConfigFileName := "test_conf.xml"
+
+		ConfigFileName := "/usr/local/etc/vk-researcher/test_conf.xml"
+		//ConfigFileName := "test_conf.xml"
 
 		xmlData, err := os.ReadFile(ConfigFileName)
 
@@ -88,8 +87,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		for i, group := range groupsSlice {
-			fmt.Printf("%d. %s (ID: %d, Участников: %d)\n", i+1, group.Name, group.ID, group.MembersCount)
+		for _, group := range groupsSlice {
 			// запрос на добавление группы
 			channelID, err := sendRequests.AddVKChannel(group, sourceID)
 			if err != nil {
@@ -97,35 +95,37 @@ func main() {
 				continue // Пропустить группу, если ошибка
 			}
 
-			fmt.Printf("---------------------------------------1\n")
-			postsSlice, _ := vk.GetGroupPosts(string(accessToken), group.ID, conf.Post_limit)
-			fmt.Printf("---------------------------------------2 len(postsSlice)=%d\n", len(postsSlice))
+			postsSlice, err := vk.GetGroupPosts(string(accessToken), group.ID, conf.Post_limit)
+			if err != nil {
+				fmt.Printf("Failed to get posts: %v\n", err)
+				continue
+			}
 
 			// TODO: отправить запросы на из добавление на сервер
 			for j, post := range postsSlice {
-				timer2 := time.NewTimer(time.Duration(2) * time.Second)
-				<-timer2.C
-				//fmt.Printf("---------------------------------------%d. %s [%d] %d\n", j+1, post.Text, post.Comments, post.Likes)
 				// запрос на добавление постов
 				postID, err := sendRequests.AddVKPost(post, channelID, nil)
 				if err != nil {
 					fmt.Printf("Failed to add post [%d]%d(%d): %v\n", j, post.ID, postID, err)
 					continue
 				}
-				fmt.Printf("added post %d", postID)
-
 				// получить комментарии
-				/*commentsSlice, _ := vk.GetCommentsWithThreads(string(accessToken), post.AuthorID, post.ID, conf.Comment_limit)
+				commentsSlice, err := vk.GetCommentsWithThreads(string(accessToken), post.AuthorID, post.ID, conf.Comment_limit)
+				if err != nil {
+					fmt.Printf("Failed to get comments: %v\n", err)
+					continue
+				}
 				// отправить на сервер
-				for k, comment := range commentsSlice {
-					fmt.Printf("=================================%d. %s %s\n", k+1, comment.AuthorName, comment.Text)
+				for _, comment := range commentsSlice {
 					// запрос на добавление комментария
 					err := sendRequests.AddVKComment(comment, postID, nil)
 					if err != nil {
 						fmt.Printf("Failed to add comment: %v\n", err)
 					}
 				}
+
 				MediaSlice, _ := vk.GetMediaFromPosts(string(accessToken), post.AuthorID, conf.Media_limit)
+				fmt.Printf("=================================len(MediaSlice)=%d\n", len(MediaSlice))
 				// запрос на добавление медиа
 				for k, mediaInfo := range MediaSlice {
 					fmt.Printf("---------------------%d. %s %s\n", k+1, mediaInfo.Type, mediaInfo.URL)
@@ -133,7 +133,7 @@ func main() {
 					if err != nil {
 						fmt.Printf("Failed to add media: %v\n", err)
 					}
-				}*/
+				}
 			}
 
 			// получить медиа
