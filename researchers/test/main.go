1ac03dd87417acd0e24092cee39b5d719dd50c80
WolfishTone <igorion98@gmail.com>
2025-11-19 01:14:46 +0700

    добавлены горутины

--- researchers/test/main.go
+++ researchers/test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 
 	"researcher-test/internal/sendRequests"
 	"researcher-test/internal/vk"
@@ -19,64 +20,71 @@ func main() {
 		fmt.Printf("sourceID = %d\n\n", sourceID)
 	}
 
-	for range 100 {
-		// сгенерировать список групп
-		var groupsSlice []vk.VKGroup
+	var wg sync.WaitGroup
 
-		groupsSlice, err = vk.GetRandGroups(1000, 0)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	for i := range 1000 {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			// сгенерировать список групп
+			var groupsSlice []vk.VKGroup
 
-		for _, group := range groupsSlice {
-			// запрос на добавление группы
-			channelID, err := sendRequests.AddVKChannel(group, sourceID)
+			groupsSlice, err = vk.GetRandGroups(1000, 0)
 			if err != nil {
-				fmt.Printf("Failed to add channel for group %s: %v\n", group.Name, err)
-				continue // Пропустить группу, если ошибка
+				fmt.Println(err)
+				os.Exit(1)
 			}
 
-			postsSlice, err := vk.GetRandPosts(1000, 1)
-			if err != nil {
-				fmt.Printf("Failed to get posts: %v\n", err)
-				continue
-			}
-
-			// TODO: отправить запросы на из добавление на сервер
-			for j, post := range postsSlice {
-				// запрос на добавление постов
-				postID, err := sendRequests.AddVKPost(post, channelID, nil)
+			for _, group := range groupsSlice {
+				// запрос на добавление группы
+				channelID, err := sendRequests.AddVKChannel(group, sourceID)
 				if err != nil {
-					fmt.Printf("Failed to add post [%d]%d(%d): %v\n", j, post.ID, postID, err)
+					fmt.Printf("Failed to add channel for group %s: %v\n", group.Name, err)
+					continue // Пропустить группу, если ошибка
+				}
+
+				postsSlice, err := vk.GetRandPosts(1000, 1)
+				if err != nil {
+					fmt.Printf("Failed to get posts: %v\n", err)
 					continue
 				}
-				// получить комментарии
-				commentsSlice, err := vk.GetRandComments(1000, 10)
-				if err != nil {
-					fmt.Printf("Failed to get comments: %v\n", err)
-					continue
-				}
-				// отправить на сервер
-				for _, comment := range commentsSlice {
-					// запрос на добавление комментария
-					err := sendRequests.AddVKComment(comment, postID, nil)
-					if err != nil {
-						fmt.Printf("Failed to add comment: %v\n", err)
-					}
-				}
 
-				MediaSlice := vk.GetRandMedia(100)
-				fmt.Printf("=================================len(MediaSlice)=%d\n", len(MediaSlice))
-				// запрос на добавление медиа
-				for k, mediaInfo := range MediaSlice {
-					fmt.Printf("---------------------%d. %s %s\n", k+1, mediaInfo.Type, mediaInfo.URL)
-					err := sendRequests.AddVKMedia(mediaInfo, postID)
+				// TODO: отправить запросы на из добавление на сервер
+				for j, post := range postsSlice {
+					// запрос на добавление постов
+					postID, err := sendRequests.AddVKPost(post, channelID, nil)
 					if err != nil {
-						fmt.Printf("Failed to add media: %v\n", err)
+						fmt.Printf("Failed to add post [%d]%d(%d): %v\n", j, post.ID, postID, err)
+						continue
+					}
+					// получить комментарии
+					commentsSlice, err := vk.GetRandComments(1000, 10)
+					if err != nil {
+						fmt.Printf("Failed to get comments: %v\n", err)
+						continue
+					}
+					// отправить на сервер
+					for _, comment := range commentsSlice {
+						// запрос на добавление комментария
+						err := sendRequests.AddVKComment(comment, postID, nil)
+						if err != nil {
+							fmt.Printf("Failed to add comment: %v\n", err)
+						}
+					}
+
+					MediaSlice := vk.GetRandMedia(100)
+					fmt.Printf("=================================len(MediaSlice)=%d\n", len(MediaSlice))
+					// запрос на добавление медиа
+					for k, mediaInfo := range MediaSlice {
+						fmt.Printf("---------------------%d. %s %s\n", k+1, mediaInfo.Type, mediaInfo.URL)
+						err := sendRequests.AddVKMedia(mediaInfo, postID)
+						if err != nil {
+							fmt.Printf("Failed to add media: %v\n", err)
+						}
 					}
 				}
 			}
-		}
+		}(i)
 	}
+	wg.Wait()
 }
